e90da790d3b9ec8280650f507591cd14a03d5775
RiumeCarlosA <riumecarlos@hotmail.com>
2023-04-20 19:29:02 +0100

    criando Sprite

---
+++ src/br/com/riume/starship/sprite/Sprite.java
@@ -0,0 +1,71 @@
+package br.com.riume.starship.sprite;
+
+import java.awt.Image;
+
+public class Sprite {
+	
+	private boolean visible;
+	private Image image;
+	private boolean dying;
+	
+	int x;
+	int y;
+	int dx;
+	
+	public Sprite() {
+		visible = true;
+	}
+	
+	public void die() {
+		visible = false;
+	}
+	
+	public boolean isVisible() {
+		return visible;
+	}
+	
+	protected void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	
+	public void setImage(Image image) {
+		this.image = image;
+	}
+	
+	public Image getImage() {
+		return image;
+	}
+
+	public boolean isDying() {
+		return dying;
+	}
+
+	public void setDying(boolean dying) {
+		this.dying = dying;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getDx() {
+		return dx;
+	}
+
+	public void setDx(int dx) {
+		this.dx = dx;
+	}
+
+}
